bd410278544864e730e7088b617dc611a011cf37
Hooly-Gun <Fasper.dev@yandex.ru>
2013-02-26 16:15:59 +0400

    route

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Fasper::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
-#  root :to => "home#index"
-#  devise_for :users
-#  resources :users
+ # devise_for :users
+   resources :users
+   root :to => "home#index"
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, :only => [:index, :destroy]
-  root :to => 'users#index'
+   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+ # resources :users, :only => [:index, :destroy]
+ # root :to => 'users#index'
 
 
 end
