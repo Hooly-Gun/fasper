403333b1c06bc3fb76b571fa1abae75a83b94a83
Hooly-Gun <Fasper.dev@yandex.ru>
2013-02-27 15:17:13 +0400

    123

--- config/routes.rb
+++ config/routes.rb
@@ -25,17 +25,42 @@
 
 
 Fasper::Application.routes.draw do
-
   get "omniauth_callbacks/facebook"
+
   get "omniauth_callbacks/vkontakte"
+
+  resources :orders
+
 
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
+#  root :to => "home#index"
+#  devise_for :users
+#  resources :users
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, :only => [:index, :destroy]
+  root :to => 'users#index'
+
 end
+
+
+
+
+
+# Fasper::Application.routes.draw do
+
+#   get "omniauth_callbacks/facebook"
+#   get "omniauth_callbacks/vkontakte"
+
+#   authenticated :user do
+#     root :to => 'home#index'
+#   end
+#   root :to => "home#index"
+#   #devise_for :users
+#   resources :users
+
+#   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+#   resources :users, :only => [:index, :destroy]
+# end
